7a1853d63708c8266bc84f832d325ce6db77f0f9
Alvaro <amasitsa@live.com>
2016-02-26 11:29:16 +0300

    changes

--- js/app.js
+++ js/app.js
@@ -19,9 +19,13 @@ var Login_service = function() {
         return $.ajax({url: request, data: form_data, type: 'POST', processData: false,contentType: false});
     }
 
-    this.login_member = function(member_no, password) {
+    /*this.login_member = function(member_no, password) {
 		var request = url + "login/login_member/" + member_no + "/" + password;
         return $.ajax({url: request});
+    }*/
+    this.login_member = function(form_data) {
+  		var request = url + "login/login_member";
+        return $.ajax({url: request, data: form_data, type: 'POST', processData: false,contentType: false});
     }
     this.get_member_details = function(member_no){
     	var request = url + "login/get_member_information/" + member_no;
@@ -29,8 +33,49 @@ var Login_service = function() {
     }
   
 }
-$("#login_member").submit(function(e) {
-   e.preventDefault();
+
+$(document).on("submit","form#login_member",function(e)
+{
+	e.preventDefault();
+	
+	//get form values
+	var form_data = new FormData(this);
+	
+	
+	//check if there is a network connection
+	//var connection = checkConnection();
+	var connection = "connected";
+	
+	if(connection != 'No network connection')
+	{
+		var service = new Login_service();
+		service.initialize().done(function () {
+			console.log("Service initialized");
+		});
+		
+		service.login_member(form_data).done(function (employees) {
+			var data = jQuery.parseJSON(employees);
+			
+			if(data.message == "success")
+			{
+				//set local variables for future auto login
+				
+				//myApp.closeModal('.popup-profup');
+				//mainView.router.loadPage('professionals.html');
+				alert(data.message);
+			}
+			else
+			{
+				alert(data.result);
+			}
+        });
+	}
+	
+	else
+	{
+		alert("No internet connection - please check your internet connection then try again");
+	}
+	return false;
 });
 
 
